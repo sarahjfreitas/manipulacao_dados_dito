46be19545f24e2cae154c50746c629d2867dbb1a
Sarah Freitas <sarahjfreitas@gmail.com>
2019-04-09 15:51:55 -0300

    response e exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  include ExceptionHandler
+  # renderiza resposta em json com o status
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
 end
